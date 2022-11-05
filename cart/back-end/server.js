8f518fe9b72a7254a1ae1ee81249193bad259ed2
Ty Klabacka <klabacka.ty@gmail.com>
2022-11-05 16:45:15 -0600

    implemented post and GET ID

--- cart/back-end/server.js
+++ cart/back-end/server.js
@@ -14,17 +14,15 @@ let products = [];
 let qty = 0;
 let uniqueId = 0;
 app.get('/api/products/:id', (req, res) => {
-    let newId = null; //req.params.id;
-    products.forEach((itm) => {
-        if (itm.id == req.params.id){
-            res.status(200).send("SUCCESS");
-            return;
-        }
-    })
+    let found = products.find((el) => el.name === req.body.name);
+    if (found){
+        res.status(200).send("SUCCESS!");
+    }
     res.status(404).send("ERROR, ID not in products");
 })
 
 app.post('/api/products', (req, res) => {
+    console.log("HERE!");
     let names = products.map((el) => {
         return el.name;
     })
@@ -38,7 +36,6 @@ app.post('/api/products', (req, res) => {
         products.push(object);
         uniqueId++;
     }
-    qty++;
     res.send(products);
 });
 
--- cart/front-end/src/App.js
+++ cart/front-end/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [apiResponse, setApiResponse] = useState('');
+  const [textVal, setTextVal] = useState('');
+  function handleSubmit(e){
+    e.preventDefault();
+    axios.post('/api/products', {name: "ty", price: "45"})
+    .then(response => {
+      console.log(response.data);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input value={textVal} onChange={(e)=>{setTextVal(e.target.value)}} />
+      </form>
+    </>
   );
 }
 
