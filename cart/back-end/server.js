87c665bae1994cfeca374df61a6279555e4508df
Ty Klabacka <klabacka.ty@gmail.com>
2022-11-05 18:05:04 -0600

    all tests passing for products on the back-end

--- cart/back-end/server.js
+++ cart/back-end/server.js
@@ -13,30 +13,39 @@ app.use(bodyParser.json());
 let products = [];
 let qty = 0;
 let uniqueId = 0;
+app.get('/api/products', (req, res) => {
+    console.log("GET!");
+    res.status(200).send(products);
+})
+
 app.get('/api/products/:id', (req, res) => {
-    let found = products.find((el) => el.name === req.body.name);
-    if (found){
-        res.status(200).send("SUCCESS!");
-    }
-    res.status(404).send("ERROR, ID not in products");
+    console.log("GETID!");
+    let found = products.find((el) => el.id == req.params.id);
+        res.status(200).send(found);
 })
 
 app.post('/api/products', (req, res) => {
-    console.log("HERE!");
-    let names = products.map((el) => {
-        return el.name;
-    })
-    let pos = names.indexOf(req.body.name);
-    if (pos === -1){
-        let object = {
-            id: uniqueId,
-            name: req.body.name,
-            price: req.body.price
-        };
-        products.push(object);
-        uniqueId++;
-    }
-    res.send(products);
+    console.log("POST!");
+    // let names = products.map((el) => {
+    //     return el.name;
+    // })
+    // let pos = names.indexOf(req.body.name);
+    // if (pos === -1){
+    let object = {
+        id: uniqueId,
+        name: req.body.name,
+        price: req.body.price
+    };
+    products.push(object);
+    uniqueId++;
+    // }
+    res.status(200).send(object);
 });
+
+app.delete('/api/products/:id', (req, res) => {
+    console.log("DELETE!");
+    products = products.filter(obj => obj.id != req.params.id)
+    res.status(200).send(products);
+})
 
 app.listen(3000, () => {console.log("LISTENING ON PORT 3000")});
